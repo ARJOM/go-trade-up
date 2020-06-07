4dae7839481244d23cd969e2a0b1ad7580950ce6
Ricart <ricarteusuo11@gmail.com>
2020-06-06 21:48:55 -0300

    Commit inicial

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./Router');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(3333, () => console.log('Rodando em http://localhost:3000'));
---
+++ frontend/src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                {/*General routes*/}
+
+                {/*Administration routes */}
+
+                {/*Error route*/}
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
