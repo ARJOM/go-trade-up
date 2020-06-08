f96746b3da9b5332b49e8c5ad375e2c2d6dae751
Marcio Fernandes Dos Santos <farciomernandes@gmail.com>
2020-06-08 14:59:43 -0300

    feat(bakend): adiciona rota para criação de usuário.

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,4 +10,4 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(3333, () => console.log('Rodando em http://localhost:3000'));
+app.listen(3333, () => console.log('Rodando em http://localhost:3333'));
