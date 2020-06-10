a1f42ef9436dd06b64f94060ceb5772f2430e4ae
Ricart <ricarteusuo11@gmail.com>
2020-06-09 21:16:22 -0300

    feat(backend): adiciona edição de usuário

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,7 +1,7 @@
 const connection = require('../database/Connection');
 
 module.exports ={
-    
+
     async create(req, res){
         const {email, user_name, password } = req.body;
 
@@ -10,17 +10,32 @@ module.exports ={
                 user_name,
                 password
             })
-            .catch(err=>console.log({procedimento: 'Listar Usuários', Status: 'Erro ao tentar Listar', Error: err}))
-            
+            .catch(err=>console.log({procedimento: 'Criar Usuários', Status: 'Erro ao tentar criar', Error: err}));
+
             return res.json(`Usuário criado com sucesso!`)
-        
+
+    },
+
+    async update(req, res){
+        const {email, user_name, password } = req.body;
+
+        const user = await connection('users')
+            .where({email: email})
+            .update({
+                user_name,
+                password
+            })
+            .catch(err=>console.log({procedimento: 'Atualizar Usuários', Status: 'Erro ao tentar atualizar', Error: err}));
+
+        return res.json(user);
+
     },
 
     async index(req, res){
 
         const users = await connection('users').select('*')
-        .catch(err=>console.log({procedimento: 'Listar Usuários', Status: 'Erro ao tentar Listar', Error: err}))
-        
+        .catch(err=>console.log({procedimento: 'Listar Usuários', Status: 'Erro ao tentar Listar', Error: err}));
+
         return res.json(users)
 
     }
--- backend/src/routes/UserRoutes.js
+++ backend/src/routes/UserRoutes.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.create);
+routes.put('/users', UserController.update);
 
 module.exports = routes;
