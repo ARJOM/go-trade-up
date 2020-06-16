4a6e69883189684dfd108711a0cb6a4d52e0d087
Marcio Fernandes Dos Santos <farciomernandes@gmail.com>
2020-06-15 23:29:07 -0300

    alteração: chave primaria se tornou agora um ID do tipo string.

--- backend/src/database/migrations/20200608115014_products.js
+++ backend/src/database/migrations/20200608115014_products.js
@@ -1,13 +1,14 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('products', function(table){
+        table.string('id').primary('product_pk');
+
         table.string('market').notNullable();
         table.string('product_name').notNullable();
         table.float('price').notNullable();
         table.string('photo').notNullable();
         table.string("description").notNullable();
 
-        table.primary(['market', 'product_name'], 'product_pk');
 
         table.foreign('market', 'product_fk').references('email')
         .inTable('markets');
