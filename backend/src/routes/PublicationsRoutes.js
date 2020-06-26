f3a0d429c9fe45d60b4c56d4c76051df6d044b3a
Ricart <ricarteusuo11@gmail.com>
2020-06-26 19:45:11 -0300

    fix: adiciona função para verificar se usuário é admin

--- backend/src/routes/PublicationsRoutes.js
+++ backend/src/routes/PublicationsRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const PublicationController = require('../controllers/PublicationController');
 
-const verifyAuth = require('../services/verifyAuth');
+const verifyAdmin = require('../services/verifyAdmin');
 
 const routes = express.Router();
 
-routes.post('/publications', verifyAuth, PublicationController.create);
+routes.post('/publications', verifyAdmin, PublicationController.create);
 routes.get('/publications', PublicationController.index);
 
 module.exports = routes;
---
+++ backend/src/services/verifyAdmin.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const SECRET = require('../config/SecretJwt');
+
+module.exports = function verifyAdmin() {
+    const token = req.headers['x-access-token'];
+
+    if (!token) return res.status(401).json({ auth: false, message: 'Token não enviado.' });
+
+    jwt.verify(token, SECRET, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Falha ao autenticar token.' });
+
+        if (decoded.id!=='admin@gotradeup.com'){
+            return res.status(403).json({auth: false, message: 'Permissão negada'});
+        }
+
+        req.user_email = decoded.id;
+        next();
+    });
+};
