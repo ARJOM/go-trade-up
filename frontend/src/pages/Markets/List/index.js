547af48ffc501672bbbd329b6967d4a56393ab06
Marcio Fernandes <farciomernandes@gmail.com>
2020-06-27 02:24:15 -0300

    view: Listagem de comerciantes pagina.

--- frontend/src/pages/Markets/List/index.js
+++ frontend/src/pages/Markets/List/index.js
@@ -1,30 +1,33 @@
 import React, {useState} from 'react';
 import api from '../../../services/api';
-import './style.css';
+import './styles.css';
 
 export default function ListMarket(){
 
     return (
-        <div>
-            <div className="top">
-                <section className="title">
-                    <h1> Comerciantes </h1>
+        <div className="listMarket">
+            <div>
+                <section>
+                    <h1 className="titulo"> Comerciantes </h1>
                 </section>
             </div>
 
-            <section className="content">
-
-                <p><darker>Comércio: </darker>//render content//.</p>
-
-                <p><darker>Local de atuação:</darker>//render content//.</p>
-
+            <div className="content">
+            <section className="dados">
+                <p><darker>Comércio: </darker>Ricart Drogaria</p>
+                <p><darker>Local de atuação:</darker>Uraúna - PB</p>
             </section>
 
             <section className="buttons">
-                <button className="btn-listprd"> <a href=""> Ver Produtos </a> </button>
+                <button className="btn-listprd">Ver Produtos </button>
 
-                <button className="btn-contact"> <a href=""> Entrar em Contato </a> </button>
+                <button className="btn-contact"> Entrar em Contato </button>
             </section>
+
+            </div>
+
+
+         
         </div>
     )
 }
