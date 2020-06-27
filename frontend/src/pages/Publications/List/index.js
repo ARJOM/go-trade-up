f1236df7ca5c83080101b870179d5806fd10d5da
Marcio Fernandes <farciomernandes@gmail.com>
2020-06-27 03:18:43 -0300

    view: Publication List css.

--- frontend/src/pages/Publications/List/index.js
+++ frontend/src/pages/Publications/List/index.js
@@ -5,20 +5,20 @@ import './style.css';
 export default function ListPublication(){
 
     return (
-        <div>
-            <div className="top">
-                <section className="title">
-                    <h1> Portal de Doações </h1>
+        <div className="listMarket">
+            <div>
+                <section>
+                    <h1 className="titulo"> Portal de Doações </h1>
                 </section>
             </div>
 
-            <section className="content">
-
-                <img src="" alt="No images available"> </img>
-
-                <p> <dark> //render content// </dark> </p>
-
+            <div className="content">
+            <section className="dados">
+                <p>Doação de R$ 150,00 direcionada para o lar de idosos
+                    de Cajazeiras-PB.</p>
+                    <p><darker>Data:</darker>27/06/2020</p>
             </section>
+            </div>
 
         </div>
     )
