4e13ab445515fcc375bb5adc70dcd18907eba390
Ricart <ricarteusuo11@gmail.com>
2020-06-27 20:49:45 -0300

    fix: adiciona arquivo de routes ao App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
-import Home from "./pages/Home";
 import Foooter from './components/Foooter';
 import './global.css'
+import Routes from "./Routes";
 
 export default function App() {
   return (
     <div>
     <Dashboard>
-      <Home />
+      <Routes/>
       <Foooter/>
     </Dashboard>
     </div>
