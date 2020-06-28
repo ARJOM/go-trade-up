03f900ae5cf92a1c30d850a1f24a6061a3257561
Ricart <ricarteusuo11@gmail.com>
2020-06-27 21:44:15 -0300

    feat: adiciona verificação de usuário logado para renderizar modal

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -1,23 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from '../../assets/logo.png';
 import './styles.css'
+
+import {isAuthenticated} from "../../utils/auth";
+
 import { Modal } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 
+
 function getModalStyle() {
     const top = 50;
     const left = 50;
-  
+
     return {
       top: `${top}%`,
       left: `${left}%`,
       transform: `translate(-${top}%, -${left}%)`,
     };
-  }
-  
-  const useStyles = makeStyles((theme) => ({
+}
+
+const useStyles = makeStyles((theme) => ({
     modal: {
         display: 'flex',
         alignItems: 'center',
@@ -40,22 +44,50 @@ function getModalStyle() {
       padding: 60,
       margin: 30,
     },
-  }));
+}));
 
 export default function Dashboard(props){
     const classes = useStyles();
-    const [modalStyle] = React.useState(getModalStyle);
-    const [open, setOpen] = React.useState(false);
-  
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+    const [body, setBody] = useState();
+
+    useEffect(() => {
+        if(isAuthenticated()){
+            setBody(loged)
+        } else {
+            setBody(anonymous)
+        }
+    }, []);
+
     const handleOpen = () => {
       setOpen(true);
     };
-  
+
     const handleClose = () => {
       setOpen(false);
     };
-  
-    const body = (
+
+    const loged = (
+        <div style={modalStyle} className={classes.paper}>
+            <h2 id="title"> Entre em GoTradeUp com seus dados</h2>
+            <section className="modal-form">
+
+                <section class="buttons">
+
+                    <button className="btn-cancel">
+                        Sair
+                    </button>
+
+                    <button className="btn-concluir">
+                        <a href=""> Minha Conta </a>
+                    </button>
+                </section>
+            </section>
+        </div>
+    );
+
+    const anonymous = (
       <div style={modalStyle} className={classes.paper}>
         <h2 id="title"> Entre em GoTradeUp com seus dados</h2>
         <h4 id="form-body">
@@ -66,12 +98,12 @@ export default function Dashboard(props){
                     <section className="inputs">
                         <label for="User-or-email">
                             <p>Nome de usuário ou E-mail:</p>
-                            <input type="text" className="nbreak" className="req" placeholder="User / email" value="" ></input>
+                            <input type="text" className="nbreak" className="req" placeholder="User / email" value="" />
                         </label>
 
                         <label for="Senha">
                             <p>Senha:</p>
-                            <input type="text" className="nbreak" className="req" placeholder="Senha" value=""></input>
+                            <input type="text" className="nbreak" className="req" placeholder="Senha" value=""/>
                         </label>
 
                     </section>
@@ -91,33 +123,13 @@ export default function Dashboard(props){
       </div>
     );
 
-
-    const body2 = (
-        <div style={modalStyle} className={classes.paper}>
-            <h2 id="title"> Entre em GoTradeUp com seus dados</h2>
-        <section className="modal-form">
-
-                    <section class="buttons">
-
-                        <button className="btn-cancel">
-                            Sair
-                        </button>
-
-                        <button className="btn-concluir">
-                            <a href=""> Minha Conta </a>
-                        </button>
-                    </section>
-        </section>
-    </div>
-);
-
     return(
         <div className="container">
 
         <div className="dashboard">
             <div className="photos-dashboard">
                 <span className="logo" ><img src={logo} height="60px" width="210px" /> </span>
-        
+
                 <span onClick={handleOpen}><img  className="user"  src="https://www.pngkit.com/png/detail/133-1338563_boys-wrestling-avatar-html.png" /></span>
 
                 <Modal
@@ -138,29 +150,8 @@ export default function Dashboard(props){
 
                 </Modal>
 
-                <span className="logo" ><img src={logo} height="60px" width="210px" /> </span>
-        
-                <span onClick={handleOpen}><img  className="user"  src="https://www.pngkit.com/png/detail/133-1338563_boys-wrestling-avatar-html.png" /></span>
-                
-                <Modal
-                    className={classes.modal2}
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="title"
-                    aria-describedby="logout-body"
-                    closeAfterTransition
-                    BackdropComponent={Backdrop}
-                    BackdropProps={{
-                      timeout: 500,
-                    }}
-                >
-                <Fade in={open}>
-                    {body2}
-                </Fade>
-                </Modal>
-
             </div>
-            
+
         <nav className="menu">
         <hr/>
             <div className="div">
@@ -181,15 +172,15 @@ export default function Dashboard(props){
             <div className="div">
                 <a href="#" className="nav-btn">Nova Publicação</a>
             </div>
-            
+
         </nav>
-      
+
         </div>
 
         <div className="body">
                {props.children}
         </div>
-    
+
         </div>
 
     )
