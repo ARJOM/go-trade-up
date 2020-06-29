f913da0b24eb948db9fda3565b16f4374e0362f6
Ricart <ricarteusuo11@gmail.com>
2020-06-28 21:03:55 -0300

    feat: adiciona funcionalidade de cadastro de produtos

--- frontend/src/pages/Products/Create/index.js
+++ frontend/src/pages/Products/Create/index.js
@@ -1,19 +1,42 @@
 import React, {useState} from 'react';
 import api from '../../../services/api';
+import Pictures from "../../../services/Pictures";
 
 export default function CreateProduct(){
 
     const [name, setName] = useState("");
     const [preco, setPreco] = useState("");
-    const [foto, setFoto] = useState("");
+    const [foto, setFoto] = useState(null);
     const [descricao, setDescricao] = useState("");
 
-    async function handleSubmit(){
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        const token = localStorage.getItem("token");
+        const email = localStorage.getItem("email");
+
+        const picture = await Pictures.upload({path: 'products', uid: email, file_name: name, file: foto});
+
         const data = {
-            name: name,
+            market: email,
+            product_name: name,
             price: preco,
-            photo: foto,
+            photo: picture,
             description: descricao,
+        };
+
+        try {
+            const response = await api.post('products', data, {
+                headers: {
+                    'x-access-token': token
+                }
+            });
+            alert("Produto cadastrado");
+
+        } catch (err) {
+            console.log(err);
+            alert("Falha ao registrar produto")
+
         }
     }
 
@@ -26,37 +49,35 @@ export default function CreateProduct(){
             </div>
 
             <section className="form">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <section className="inputs">
                         <label for="Nome">
                             Nome do produto:
-                            <input type="text" className="nbreak" className="req" placeholder="Nome" value={name} onChange={text=>setName(text.target.value)}></input>
+                            <input type="text" className="nbreak" className="req" placeholder="Nome" value={name} onChange={text => setName(text.target.value)}/>
                         </label>
 
                         <label for="Preco">
                             Preco do produto:
-                            <input type="text" className="break" className="req" placeholder="Preco" value={preco} onChange={text=>setPreco(text.target.value)}></input>
+                            <input type="text" className="break" className="req" placeholder="Preco" value={preco} onChange={text => setPreco(text.target.value)}/>
                         </label>
 
                         <label for="Foto">
                             Foto:
-                            <input type="image" src="" alt="No Images Available" > </input>
+                            <input type="file" src="" alt="No Images Available" onChange={e => setFoto(e.target.files[0])}/>
                         </label>
 
                         <label for="Descricao">
                             Descricao:
-                            <input type="text" className="break" className="req" placeholder="Quantidade" min="1" value={descricao} onChange={text=>setDescricao(text.target.value)}></input>
+                            <input type="text" className="break" className="req" placeholder="Quantidade" min="1" value={descricao} onChange={text => setDescricao(text.target.value)}/>
                         </label>
                     </section>
-
-                    <input type="submit" value="Alterar"></input>
 
                     <section>
                         <button className="btn-cancel">
                             Cancelar
                         </button>
 
-                        <button onClick={handleSubmit} className="btn-concluir">
+                        <button className="btn-concluir">
                             Salvar Alteracoes
                         </button>
 
