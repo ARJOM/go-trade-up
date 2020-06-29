f583b907271277089b8bdd2bcfe60575bd3292ce
Marcio Fernandes <farciomernandes@gmail.com>
2020-06-29 19:27:15 -0300

    view: Adicionando malito ao footer.

--- frontend/src/components/Foooter/index.js
+++ frontend/src/components/Foooter/index.js
@@ -6,7 +6,10 @@ export default function Foooter(){
   return(
        <div className="foooter">
                 <section>
-                    <button className="btn-concluir">CONTATO</button>
+                <button className="btn-concluir">
+                  <a href="mailto:gotradeup@gmail.com?Subject=Ol%E1%2C%20estou%20entrando%20em%20contato%20pelo%20Site.">
+                  CONTATO</a></button>
+                   
                 </section>
                 <section>
                       <p>Copyright ©2020 by Marcio Fernandes, Gabriel Alves and Antônio Ricart</p>
